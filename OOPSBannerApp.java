3857a906d01ed1e42b9b59e651d50c87c5bf0d17
spiddrr01 <as1264@srmist.edu.in>
2026-03-02 18:41:59 +0530

    Update README.md

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+/**
+ * @author Anurag
+ * @version 1.0
+ * 
+ * UC1: Print the literal text "OOPS" to the console
+ * 
+ * This is the foundational implementation that demonstrates:
+ * - The use of a Class as a container for data and logic
+ * - The Main Method as the application's entry point
+ * - The static keyword for declaring class-level methods
+ * - String Literals for text representation
+ * - Method Invocation via System.out.println()
+ */
+public class OOPSBannerApp {
+    
+    /**
+     * Main method - the entry point of the application
+     * @param args Command line arguments (not used in UC1)
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
